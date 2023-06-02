2b6dc67c282c04199184c1f9aa465c0aac022095
yakun9it <133485873+yakun9it@users.noreply.github.com>
2023-06-02 14:42:44 +0800

    add paging

--- src/components/Players.comp.js
+++ src/components/Players.comp.js
@@ -24,6 +24,91 @@ const Players = () => {
     const [value, setValue] = React.useState('1');
     const week_now = moment().isoWeek();
     //==================== all player ========================//
+    const postsPerPage = 10;
+    const [currentPage_win, setCurrentPage_win] = React.useState(1);
+    const [currentPage_tcuoc, setCurrentPage_tcuoc] = React.useState(1);
+
+    const Paging_win = () => {
+        const postsPerPage = 10;
+        const data = player_wins;
+        const pageNumbers = [];
+        const totalPosts = data.length;
+        for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+            pageNumbers.push(i)
+        }
+        const pagination = (pageNumbers) => {
+            setCurrentPage_win(pageNumbers)
+        }
+        return (
+            <ul className="bxh-pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className={currentPage_win === number ? 'bxh-paging-li active' : 'bxh-paging-li'}>
+                        <button onClick={() => pagination(number)} className="bxh-paging-button"> {number} </button>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    const showData_win = (list) => {
+        const indexOfLastPage = currentPage_win * postsPerPage;
+        const indexOfFirstPage = indexOfLastPage - postsPerPage;
+        const currentPosts = player_wins.slice(indexOfFirstPage, indexOfLastPage)
+        console.log(indexOfFirstPage + " - " + indexOfLastPage);
+        return (
+            currentPosts.map((player, index) => (
+                <tr key={player.rank + player.updateAt}>
+                    <td className="table-td">{player.rank}</td>
+                    <td className="table-td">{player.player}</td>
+                    <td className="table-td">{
+                        Number(player.result).toLocaleString('id')
+                    }</td>
+                    <td className="table-td">{player.point}</td>
+                    <td className="table-td">{player.updateAt}</td>
+                </tr>
+            ))
+        )
+    }
+    const Paging_tcuoc = () => {
+        const postsPerPage = 10;
+        const data = player_total_bets;
+        const pageNumbers = [];
+        const totalPosts = data.length;
+        for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+            pageNumbers.push(i)
+        }
+        const pagination = (pageNumbers) => {
+            setCurrentPage_tcuoc(pageNumbers)
+        }
+        return (
+            <ul className="bxh-pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className={currentPage_tcuoc === number ? 'bxh-paging-li active' : 'bxh-paging-li'}>
+                        <button onClick={() => pagination(number)} className="bxh-paging-button"> {number} </button>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    const showData_tcuoc = (list) => {
+        const indexOfLastPage = currentPage_tcuoc * postsPerPage;
+        const indexOfFirstPage = indexOfLastPage - postsPerPage;
+        const currentPosts = player_total_bets.slice(indexOfFirstPage, indexOfLastPage)
+        console.log(indexOfFirstPage + " - " + indexOfLastPage);
+        return (
+            currentPosts.map((player, index) => (
+                <tr key={player.rank + player.updateAt}>
+                    <td className="table-td">{player.rank}</td>
+                    <td className="table-td">{player.player}</td>
+                    <td className="table-td">{
+                        Number(player.result).toLocaleString('id')
+                    }</td>
+                    <td className="table-td">{player.point}</td>
+                    <td className="table-td">{player.updateAt}</td>
+                </tr>
+            ))
+        )
+    }
+    //
     let handle_get_tongcuocs = async (week_now) => { let get_tongcuocs_res = await get_tongcuoc_weeks(week_now); setPlayer_wins(get_tongcuocs_res); };
     let handle_get_thanglons = async (week_now) => { let get_thanglons_res = await get_thanglon_weeks(week_now); setPlayer_total_bets(get_thanglons_res); };
     // =================== one player search ======================= //
@@ -116,24 +201,16 @@ const Players = () => {
                                 </tr>
                             </thead>
                             <tbody className="table-tbody">
-                                {player_wins.length > 0 && player_wins.map((player, index) => (
-                                    <tr key={player.rank + player.updateAt}>
-                                        <td className="table-td">{player.rank}</td>
-                                        <td className="table-td">{player.player}</td>
-                                        <td className="table-td">{
-                                            Number(player.result).toLocaleString('id')
-                                        }</td>
-                                        <td className="table-td">{player.point}</td>
-                                        <td className="table-td">{player.updateAt}</td>
-                                    </tr>
-                                ))
-                                }
-                                {player_wins.length === 0 &&
+                                {player_total_bets.length > 0 && showData_tcuoc()}
+                                {player_total_bets.length === 0 &&
                                     <tr>
                                         <td colSpan={5} className="no-data">Không có dữ liệu xếp hạng!</td>
                                     </tr>
                                 }
                             </tbody>
+                            <div>
+                                {Paging_tcuoc()}
+                            </div>
                         </table>
                     </TabPanel>
                     <TabPanel value="2" className="panel-table">
@@ -148,24 +225,16 @@ const Players = () => {
                                 </tr>
                             </thead>
                             <tbody className="table-tbody">
-                                {player_total_bets.length > 0 && player_total_bets.map((player, index) => (
-                                    <tr key={player.rank + player.updateAt}>
-                                        <td className="table-td">{player.rank}</td>
-                                        <td className="table-td">{player.player}</td>
-                                        <td className="table-td">{
-                                            Number(player.result).toLocaleString('id')
-                                        }</td>
-                                        <td className="table-td">{player.point}</td>
-                                        <td className="table-td">{player.updateAt}</td>
-                                    </tr>
-                                ))
-                                }
-                                {player_total_bets.length === 0 &&
+                                {player_wins.length > 0 && showData_win()}
+                                {player_wins.length === 0 &&
                                     <tr>
                                         <td colSpan={5} className="no-data">Không có dữ liệu xếp hạng!</td>
                                     </tr>
                                 }
                             </tbody>
+                            <div>
+                                {Paging_win()}
+                            </div>
                         </table>
                     </TabPanel>
                 </TabContext>
